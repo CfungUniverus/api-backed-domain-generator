973cdb7ccbea4d6479b7d39ea771b64599e02fb3
Cameron Fung <cfung@univerus.com>
2023-12-14 13:37:53 -0800

    removed home page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ObjectFieldInfoForm from './ObjectFieldInfoForm'; 
 import ObjectInfoForm from './ObjectInfoForm';
+import { Box, Typography, dividerClasses } from '@mui/material';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/field" element={<ObjectFieldInfoForm />} />
-        <Route path="/" element={<ObjectInfoForm />} />
-      </Routes>
-    </Router>
+<>
+<Typography variant={"h3"} sx={{textAlign: "center"}}>Generating API Backed Domains</Typography>
+
+<Box sx={{display: "flex", flexDirection: "row", gap: "40px"}}> 
+      <ObjectInfoForm/>
+      <ObjectFieldInfoForm/>
+    </Box>
+</>
   );
 }
 
